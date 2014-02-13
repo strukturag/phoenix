434ce0193fef7100c271335d6344490696977160
Lance Cooper <lance@struktur.de>
2014-02-12 18:52:12 -0600

    Add licencing information to all files.

--- doc.go
+++ doc.go
@@ -1,3 +1,7 @@
+// Copyright 2014 struktur AG. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package phoenix provides runtime support for long running server processes.
 //
 // In particular, it provides standardized mechanisms for handling logging,
